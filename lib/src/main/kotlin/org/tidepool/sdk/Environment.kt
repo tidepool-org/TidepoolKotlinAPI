bc3fd03250c9c394aa3cfd3c80a87e4d5dad4b98
kylecooney <kylec@tidepool.org>
2024-08-05 17:37:17 -0700

    Fix environment realm ids

--- lib/src/main/kotlin/org/tidepool/sdk/Environment.kt
+++ lib/src/main/kotlin/org/tidepool/sdk/Environment.kt
@@ -12,8 +12,8 @@ interface Environment {
 }
 
 public enum class Environments(url: String, override val envCode: String, override val auth: AuthenticationServer) : Environment {
-	Production("https://api.tidepool.org", "prod", AuthenticationServers.Production),
-	Integration("https://external.integration.tidepool.org", "int", AuthenticationServers.External),
+	Production("https://api.tidepool.org", "tidepool", AuthenticationServers.Production),
+	Integration("https://external.integration.tidepool.org", "integration", AuthenticationServers.External),
 	Dev1("https://dev1.dev.tidepool.org", "dev", AuthenticationServers.Development),
 	Qa1("https://qa1.development.tidepool.org", "qa1", AuthenticationServers.QA),
 	Qa2("https://qa2.development.tidepool.org", "qa2", AuthenticationServers.QA);
