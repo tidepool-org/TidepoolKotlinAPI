48dc18efc65960fa9a52d46ca0e39fdfbc7ede18
kylecooney <kylec@tidepool.org>
2024-08-07 14:08:32 -0700

    Add wrapper for glucose readings

--- lib/src/main/kotlin/org/tidepool/sdk/model/BloodGlucose.kt
+++ lib/src/main/kotlin/org/tidepool/sdk/model/BloodGlucose.kt
@@ -14,8 +14,66 @@ public class BloodGlucose {
 			if (this == units) {
 				return amount
 			}
-			return amount * units.value * value
+			return amount * units.value / value
 		}
+	}
+
+	public class GlucoseReading(val amount: Double, val units: Units): Comparable<GlucoseReading> {
+		public fun inUnit(newUnit: Units): Double {
+			return units.convert(amount, newUnit)
+		}
+		
+		operator fun unaryPlus(): GlucoseReading = copy(amount = +this.amount)
+		operator fun unaryMinus(): GlucoseReading = copy(amount = -this.amount)
+		operator fun plus(other: GlucoseReading): GlucoseReading = copy(amount = amount + other.inUnit(units))
+		operator fun minus(other: GlucoseReading): GlucoseReading = copy(amount = amount - other.inUnit(units))
+		public fun copy(amount: Double = this.amount, units: Units = this.units) = GlucoseReading(amount, units)
+
+		override fun equals(other: Any?): Boolean {
+			if (this === other) return true
+			if (other !is GlucoseReading) return false
+			return amount.equals(other.inUnit(units))
+		}
+
+		override fun hashCode(): Int {
+			return inUnit(Units.milligramsPerDeciliter).hashCode()
+		}
+
+  		override fun compareTo(other: GlucoseReading): Int {
+			return amount.compareTo(other.inUnit(units))
+		}
+	}
+
+	public fun Int.mgdl(): GlucoseReading {
+		return GlucoseReading(toDouble(), Units.milligramsPerDeciliter)
+	}
+
+	public fun Long.mgdl(): GlucoseReading {
+		return GlucoseReading(toDouble(), Units.milligramsPerDeciliter)
+	}
+
+	public fun Float.mgdl(): GlucoseReading {
+		return GlucoseReading(toDouble(), Units.milligramsPerDeciliter)
+	}
+
+	public fun Double.mgdl(): GlucoseReading {
+		return GlucoseReading(this, Units.milligramsPerDeciliter)
+	}
+
+	public fun Int.mmoll(): GlucoseReading {
+		return GlucoseReading(toDouble(), Units.millimolesPerLiter)
+	}
+
+	public fun Long.mmoll(): GlucoseReading {
+		return GlucoseReading(toDouble(), Units.millimolesPerLiter)
+	}
+
+	public fun Float.mmoll(): GlucoseReading {
+		return GlucoseReading(toDouble(), Units.millimolesPerLiter)
+	}
+
+	public fun Double.mmoll(): GlucoseReading {
+		return GlucoseReading(this, Units.millimolesPerLiter)
 	}
 
 	enum class Trend {
--- lib/src/main/kotlin/org/tidepool/sdk/model/data/ContinuousGlucoseData.kt
+++ lib/src/main/kotlin/org/tidepool/sdk/model/data/ContinuousGlucoseData.kt
@@ -1,10 +1,24 @@
 package org.tidepool.sdk.model.data
 
 import org.tidepool.sdk.model.BloodGlucose
+import org.tidepool.sdk.model.BloodGlucose.GlucoseReading
 
 public data class ContinuousGlucoseData(
 	val value: Double? = null,
 	val units: BloodGlucose.Units? = null,
 	val trend: BloodGlucose.Trend? = null,
 	val trendRate: Double? = null
-): BaseData(type=BaseData.DataType.cbg)
+): BaseData(type=BaseData.DataType.cbg) {
+	
+	public constructor(reading: GlucoseReading?, trend: BloodGlucose.Trend?, trendRate: Double?) : this(reading?.amount, reading?.units, trend, trendRate)
+	
+	val reading: GlucoseReading? by lazy {
+		value?.let { value ->
+			units?.let { units ->
+				GlucoseReading(value, units)
+			}
+		}
+	}
+
+	fun copy(reading: GlucoseReading? = this.reading, trend: BloodGlucose.Trend? = this.trend, trendRate: Double? = this.trendRate) = copy(reading?.amount, reading?.units, trend, trendRate)
+}
