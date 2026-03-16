9f371b0a5713a649604397ccb7dbca430f26095d
Petr David <petrdavid9@gmail.com>
2026-03-16 14:28:24 +0100

    [NEMO-256] fix missing carb uploads

---
+++ domain/src/commonMain/kotlin/org/tidepool/sdk/model/data/EnergyUnit.kt
@@ -0,0 +1,8 @@
+package org.tidepool.sdk.model.data
+
+enum class EnergyUnit {
+    Calories,
+    Joules,
+    Kilocalories,
+    Kilojoules,
+}
---
+++ domain/src/commonMain/kotlin/org/tidepool/sdk/model/data/NutrientUnit.kt
@@ -0,0 +1,5 @@
+package org.tidepool.sdk.model.data
+
+enum class NutrientUnit {
+    Grams,
+}
